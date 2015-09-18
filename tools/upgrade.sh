397ba52aa83c995e1116546b748ea6ebb0e650e5
Wajdi Al-Hawari <w.hawari@gmail.com>
2015-09-18 08:07:12 -0400

    safe commit...not working yet...trying to intelligently toggle between branches safely...not cool

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,17 +1,8 @@
 printf '\033[0;34m%s\033[0m\n' "Upgrading Oh My Zsh"
 cd "$ZSH"
 
-remote="origin"
-
-if $FORKED_REPO
-then
- remote="upstream"
-  git stash
-fi
-
-if git pull --rebase --stat "$remote" master
-then
-    printf '\033[0;32m%s\033[0m\n' '         __                                     __   '
+function _successful_upgrade() {
+  printf '\033[0;32m%s\033[0m\n' '         __                                     __   '
     printf '\033[0;32m%s\033[0m\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
     printf '\033[0;32m%s\033[0m\n' ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
     printf '\033[0;32m%s\033[0m\n' '/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '
@@ -20,11 +11,39 @@ then
     printf '\033[0;34m%s\033[0m\n' 'Hooray! Oh My Zsh has been updated and/or is at the current version.'
     printf '\033[0;34m%s\033[1m%s\033[0m\n' 'To keep up on the latest news and updates, follow us on twitter: ' 'https://twitter.com/ohmyzsh'
     printf '\033[0;34m%s\033[1m%s\033[0m\n' 'Get your Oh My Zsh swag at: ' 'http://shop.planetargon.com/'
+}
+
+function _upgrade_failed() {
+ printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
+}
+
+remote="origin"
+
+if $FORKED_REPO
+then
+ remote="upstream"
+ current_branch="$(git rev-parse --abbrev-ref HEAD)"
+
+ git stash
+
+ if [ "$current_branch" -ne "master" ]
+ then
+  git checkout master
+ fi
+fi
+
+if git pull --rebase --stat "$remote" master
+then
+   _successful_upgrade
   else
-    printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
+  _upgrade_failed
 fi
 
 if $FORKED_REPO
 then
+ if [ "$current_branch" -ne "master" ]
+ then
+  git checkout "$current_branch"
+ fi
  git stash pop
 fi
